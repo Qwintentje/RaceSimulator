612c0c7e379ca0e06d47e0737073907ce6982f35
Quinten van Nimwegen <Quintenvnimwegen@gmail.com>
2022-09-14 16:30:04 +0200

    tot en met 2.1

---
+++ Model/Competition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class Competition
+    {
+        public List<IParticipant> Participants { get; set; }
+        public Queue<Track> Tracks { get; set; }
+        public Track NextTrack()
+        {
+            return null;
+        }
+    }
+}
---
+++ Model/SectionData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class SectionData
+    {
+        public IParticipant Left{ get; set; }
+        public int DistanceLeft { get; set; }
+        public IParticipant Right { get; set; }
+        public int DistanceRight { get; set; }
+
+    }
+}
