4617b7beccec78f4255b5d5091b2fa16e09255f8
Quinten van Nimwegen <Quintenvnimwegen@gmail.com>
2022-09-15 12:32:00 +0200

    Level 2-4

---
+++ Controller/Race.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class Race
+    {
+        public Track Track { get; set; }
+        public List<IParticipant> Participants { get; set; }
+        public DateTime StartTime { get; set; }
+        private Random _random;
+        private Dictionary<Section, SectionData> _positions;
+
+        public SectionData GetSectionData(Section section)
+        {
+            if (_positions[section] != null)
+            {
+                return _positions[section];
+            }
+            else
+            {
+                _positions.Add(section, new SectionData());
+                return _positions[section];
+            }
+        }
+        public Race(Track track, List<IParticipant> participants)
+        {
+            Track = track;
+            Participants = participants;
+            _random = new Random(DateTime.Now.Millisecond);
+        }
+
+        public void RandomizeEquipment()
+        {
+
+        }
+    }
+}
